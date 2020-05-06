ef361a10ebdf5b6d9d4ae039eb8b6d84522e1e56
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 01:33:03 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  reverse_array =[]
+  new_array.each do |word|
+    reverse_array.push(word.reverse)
+  end 
+  return reverse_array.join(" ")
